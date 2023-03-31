a23534c5a1d9f64b7181f55e6ed47ffb4397b8c5
Neelima Gundu <67305099+NeelimaGundu@users.noreply.github.com>
2023-03-31 19:17:05 +0530

    Update submission.component.ts

--- src/app/submission/submission.component.ts
+++ src/app/submission/submission.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {FlatTreeControl} from '@angular/cdk/tree';
-import {MatTreeFlatDataSource, MatTreeFlattener} from '@angular/material/tree';
+import { FlatTreeControl } from '@angular/cdk/tree';
+import { MatTreeFlatDataSource, MatTreeFlattener } from '@angular/material/tree';
+import { FormBuilder, FormGroup, Validators, NgForm } from '@angular/forms';
+import { ManuscriptService } from '../manuscript.service';
+
 interface FoodNode {
   name: string;
   children?: FoodNode[];
@@ -9,15 +12,15 @@ const TREE_DATA: FoodNode[] = [
   {
     name: '2021',
     children: [
-      {name: 'Volume 1, Issue 1'},
+      { name: 'Volume 1, Issue 1' },
 
     ]
   }];
-  interface ExampleFlatNode {
-    expandable: boolean;
-    name: string;
-    level: number;
-  }
+interface ExampleFlatNode {
+  expandable: boolean;
+  name: string;
+  level: number;
+}
 
 
 @Component({
@@ -27,7 +30,12 @@ const TREE_DATA: FoodNode[] = [
 })
 export class SubmissionComponent implements OnInit {
 
-  constructor() { this.dataSource.data = TREE_DATA;}
+  adminForm: FormGroup;
+  submitted = false;
+  upload: File;
+  articleData: any = {};
+  constructor(private data: ManuscriptService, private formBuilder: FormBuilder) { this.dataSource.data = TREE_DATA; }
+  get f() { return this.adminForm.controls; }
   private _transformer = (node: FoodNode, level: number) => {
     return {
       expandable: !!node.children && node.children.length > 0,
@@ -36,16 +44,43 @@ export class SubmissionComponent implements OnInit {
     };
   }
 
+
   treeControl = new FlatTreeControl<ExampleFlatNode>(
-      node => node.level, node => node.expandable);
+    node => node.level, node => node.expandable);
 
   treeFlattener = new MatTreeFlattener(
-      this._transformer, node => node.level, node => node.expandable, node => node.children);
+    this._transformer, node => node.level, node => node.expandable, node => node.children);
 
   dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
   hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
 
+
+
   ngOnInit(): void {
+    this.adminForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      phoneno: ['', Validators.required],
+      articlename: ['', Validators.required],
+      authors: ['', Validators.required],
+      aboutauthor: ['', Validators.required],
+      keywords: ['', Validators.required],
+      abstract: ['', Validators.required],
+      upload: ['', [Validators.required]],
+
+    })
+  }
+
+  submit() {
+    console.log('data ' + this.adminForm.value);
+    alert("Saved Successfully")
+    //this.manudata.image=this.file;
+    this.data.articleData(this.adminForm.value);
+    let formData = new FormData();
+    formData.append("article", this.upload);
+    console.log('file data  ' + formData);
+    this.data.articleFileData(formData);
   }
 
 }
+
