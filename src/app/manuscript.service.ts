6b73ec56f73bbc0ff0132cb72344938219c2f53a
Neelima Gundu <67305099+NeelimaGundu@users.noreply.github.com>
2023-04-14 20:51:43 +0530

    Update manuscript.service.ts

--- src/app/manuscript.service.ts
+++ src/app/manuscript.service.ts
@@ -79,7 +79,7 @@ export class ManuscriptService {
 
   articleFileData(article) {
     console.log(article);
-    this.http.post<any>(this.url + '/author/articlefilesubmission', (article)).subscribe(res => {
+    this.http.post<any>(this.url + '/article/submit-article', (article)).subscribe(res => {
       console.log(res)
     },
       err => {
