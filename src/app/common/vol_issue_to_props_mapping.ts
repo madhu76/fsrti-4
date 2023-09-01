915211680339946176b35e94a7e28f1619eb1b34
Dr.G.MADHU <madhu2376@gmail.com>
2023-09-01 19:07:58 +0530

    Update vol_issue_to_props_mapping.ts

--- src/app/common/vol_issue_to_props_mapping.ts
+++ src/app/common/vol_issue_to_props_mapping.ts
@@ -42,6 +42,10 @@ const mappings = {
     "3.3": {
         "Name": "Volume 3, Issue 3, July 2023",
         "Contents": "https://res.cloudinary.com/jisst/image/upload/v1693574655/Cont03-03_aqnz1l.pdf"
+    },
+    "3.4": {
+        "Name": "Volume 3, Issue 4, October 2023",
+        "Contents": "https://res.cloudinary.com/jisst/image/upload/v1693574655/Cont03-03_aqnz1l.pdf"
     }
 }
 
