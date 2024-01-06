0f6172526c0fe444cd5807558b7afef20e8f651a
Dr.G.MADHU <madhu2376@gmail.com>
2024-01-06 15:36:54 +0530

    Update sidenav.component.ts

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -38,7 +38,7 @@ const TREE_DATA: FoodNode[] = [
   {
     name: 'Special Issues',
     children: [
-      //{name: 'Special Issue Announcement, January 2024'},
+      {name: 'Special Issue Announcement, January 2024'},
       // {name: 'Volume 3, Issue 2'},
       // {name: 'Volume 3, Issue 3'},
       // {name: 'Volume 3, Issue 4'}
