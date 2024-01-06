10ae574f23e7959666cf9003890329623e49e4c0
Dr.G.MADHU <madhu2376@gmail.com>
2024-01-06 16:26:27 +0530

    Update vol_issue_to_props_mapping.ts

--- src/app/common/vol_issue_to_props_mapping.ts
+++ src/app/common/vol_issue_to_props_mapping.ts
@@ -48,7 +48,11 @@ const mappings = {
         "Contents": "https://res.cloudinary.com/jisst/image/upload/v1703322699/Cont_V3_4_sxf0vp.pdf"
     },
     "Special Issues": {
-        "Name": "January 2024",
+        "Name": "Published",
+        "Contents": "https://res.cloudinary.com/jisst/image/upload/v1704532361/Special_Issue-Fractional_Calculus_oimau0.pdf"
+    },
+    "Special Issues": {
+        "Name": "Fractional Calculus: Call for Papers",
         "Contents": "https://res.cloudinary.com/jisst/image/upload/v1704532361/Special_Issue-Fractional_Calculus_oimau0.pdf"
     }
 }
