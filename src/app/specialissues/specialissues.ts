96d6cad8cf8f55f45faf8a2e895469e151fdbc3b
Dr.G.MADHU <madhu2376@gmail.com>
2024-01-08 15:13:47 +0530

    Update specialissues.ts

--- src/app/specialissues/specialissues.ts
+++ src/app/specialissues/specialissues.ts
@@ -1,6 +1,35 @@
 const specialissues = {
     "Published":[
-        
+        {
+            "Title": "Current Trends in Computational Modelling and Simulation-Part-1",
+            "SubmissionDeadLine":"31 January 2022",
+            "File":"https://res.cloudinary.com/jisst/image/upload/v1704706471/Cont_2-1-ver_wzbibnSpecial_Issue-1_l3ib5n.pdf",
+            "Editors":"Navnit Jha (India) . Anuradha Mahasinghe (Sri Lanka)"
+        },
+        {
+            "Title": "Current Trends in Computational Modelling and Simulation-Part-2",
+            "SubmissionDeadLine":"30 April 2022",
+            "File":"https://res.cloudinary.com/jisst/image/upload/v1704706603/Cont_2-2_Updated-Version_euchia-Special_Issue-2_mq84qg.pdf",
+            "Editors":"Navnit Jha (India) . Anuradha Mahasinghe (Sri Lanka)"
+        },
+       {
+            "Title": "Current Topics in Biological Sciences",
+            "SubmissionDeadLine":"31 July 2022",
+            "File":"https://res.cloudinary.com/jisst/image/upload/v1704706710/Cont02-03_tun7dl-Special_Issue-3_hflxp1.pdf",
+            "Editors":"Navnit Jha (India) . Iroja Caldera (Sri Lanka)"
+        },
+        {
+            "Title": "Real World Dynamical Systems and Numerics Part-1 : Finite and Infinite Dimensional Dynamical Systems",
+            "SubmissionDeadLine":"30 April 2023",
+            "File":"https://res.cloudinary.com/jisst/image/upload/v1704706838/Cont03-02_afmceh-Special_Issue-4_xnbmke.pdf",
+            "Editors":"Flank David Bezerra (Brazil). Jocirei Dias Ferreira (Brazil). Severino Hor´acio da Silva (Brazil)"
+        },
+        {
+            "Title": "Real World Dynamical Systems and Numerics Part-2 : Human Health Concerns",
+            "SubmissionDeadLine":"31 July 2023",
+            "File":"https://res.cloudinary.com/jisst/image/upload/v1704706973/Cont03-03-new_ixtlgh-Special_Issue-5_yz7gpo.pdf",
+            "Editors":"Saroj Kumar Sahani (India) . Severino Hor´acio da Silva (Brazil)"
+        }
     ],
     "Open":[
         {
