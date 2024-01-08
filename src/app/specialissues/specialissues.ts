e2f8f93260bf1093146df764e937351a79a62fc3
Dr.G.MADHU <madhu2376@gmail.com>
2024-01-08 14:51:15 +0530

    Update specialissues.ts

--- src/app/specialissues/specialissues.ts
+++ src/app/specialissues/specialissues.ts
@@ -5,11 +5,22 @@ const specialissues = {
     "Open":[
         {
             "Title": "Advances in Fractional Calculus: Theory, Models and Applications",
-            "SubmissionDeadLine":"31 December 2024",
+            "SubmissionDeadLine":"29 February 2024",
             "File":"https://res.cloudinary.com/jisst/image/upload/v1704532361/Special_Issue-Fractional_Calculus_oimau0.pdf",
             "Editors":"Rajesh K. Pandey, Indian Institute of Technology(BHU) Varanasi, India; Yufeng Xu, Central South University, Changsha, Hunan, P.R. China"
         }
-        
+        {
+            "Title": "Financial Engineering – Theory, Models and Software Implementations",
+            "SubmissionDeadLine":"30 April 2024",
+            "File":"https://res.cloudinary.com/jisst/image/upload/v1704705354/SI-Fiancial_Engg._-_Dead_line_30_April_2024_nt5ngd.pdf",
+            "Editors":"Siddhartha Pratim Chakrabarty, Indian Institute of Technology Guwahati, India, KVNM Ramesh, PES University EC Campus, Bangalore, India"
+        }
+        {
+            "Title": "Advances in Fluid Flow and Heat Transfer Studies",
+            "SubmissionDeadLine":"30 April 2024",
+            "File":"https://res.cloudinary.com/jisst/image/upload/v1704705591/Special_Issue-Fluid_Flows_Dead_Line_30_April_2024_icadvd.pdf",
+            "Editors":"T.S.L. Radhika, BITS Pilani, Hyderabad campus, India, Jyotirmoy Rana, IIT Hyderabad, India, S. Srinivas, VIT-AP University, Amaravati, India"
+        }
     ]
 }
 
