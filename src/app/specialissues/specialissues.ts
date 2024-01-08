4512566553037079ccc1cc030d83acaa3a15e5c5
Dr.G.MADHU <madhu2376@gmail.com>
2024-01-08 14:55:08 +0530

    Update specialissues.ts

--- src/app/specialissues/specialissues.ts
+++ src/app/specialissues/specialissues.ts
@@ -8,13 +8,13 @@ const specialissues = {
             "SubmissionDeadLine":"29 February 2024",
             "File":"https://res.cloudinary.com/jisst/image/upload/v1704532361/Special_Issue-Fractional_Calculus_oimau0.pdf",
             "Editors":"Rajesh K. Pandey, Indian Institute of Technology(BHU) Varanasi, India; Yufeng Xu, Central South University, Changsha, Hunan, P.R. China"
-        }
+        },
         {
             "Title": "Financial Engineering – Theory, Models and Software Implementations",
             "SubmissionDeadLine":"30 April 2024",
             "File":"https://res.cloudinary.com/jisst/image/upload/v1704705354/SI-Fiancial_Engg._-_Dead_line_30_April_2024_nt5ngd.pdf",
             "Editors":"Siddhartha Pratim Chakrabarty, Indian Institute of Technology Guwahati, India, KVNM Ramesh, PES University EC Campus, Bangalore, India"
-        }
+        },
         {
             "Title": "Advances in Fluid Flow and Heat Transfer Studies",
             "SubmissionDeadLine":"30 April 2024",
