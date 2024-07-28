fffd774a03ec6ac9a1657f19ad25bb6d2cfccdc5
Dr.G.MADHU <madhu2376@gmail.com>
2024-07-28 10:40:02 +0530

    Update specialissues.ts

--- src/app/specialissues/specialissues.ts
+++ src/app/specialissues/specialissues.ts
@@ -32,27 +32,27 @@ const specialissues = {
         }
     ],
     "Open":[
-        {
-            "Title": "Advances in Fractional Calculus: Theory, Models and Applications",
-            "SubmissionDeadLine":"29 February 2024",
-            "File":"https://res.cloudinary.com/jisst/image/upload/v1704532361/Special_Issue-Fractional_Calculus_oimau0.pdf",
-            "Editors":"Rajesh K. Pandey, Indian Institute of Technology(BHU) Varanasi, India; Yufeng Xu, Central South University, Changsha, Hunan, P.R. China"
-        },
+        // {
+        //     "Title": "Advances in Fractional Calculus: Theory, Models and Applications",
+        //     "SubmissionDeadLine":"29 February 2024",
+        //     "File":"https://res.cloudinary.com/jisst/image/upload/v1704532361/Special_Issue-Fractional_Calculus_oimau0.pdf",
+        //     "Editors":"Rajesh K. Pandey, Indian Institute of Technology(BHU) Varanasi, India; Yufeng Xu, Central South University, Changsha, Hunan, P.R. China"
+        // },
         {
             "Title": "Advances in Partial Differential Equations:Theory, Applications and Numerics",
-            "SubmissionDeadLine":"31 March 2024",
+            "SubmissionDeadLine":"31 December 2024",
             "File":"https://res.cloudinary.com/jisst/image/upload/v1704815686/S_ecial_Issue_on_PDE_s_jjjsec.pdf",
             "Editors":"Neelesh Kumar, Dyal Singh College, University of Delhi, India, Pankaj Mishra, Deshbandhu College, University of Delhi, India"
         },
         {
             "Title": "Financial Engineering – Theory, Models and Software Implementations",
-            "SubmissionDeadLine":"30 April 2024",
+            "SubmissionDeadLine":"31 December 2024",
             "File":"https://res.cloudinary.com/jisst/image/upload/v1704705354/SI-Fiancial_Engg._-_Dead_line_30_April_2024_nt5ngd.pdf",
             "Editors":"Siddhartha Pratim Chakrabarty, Indian Institute of Technology Guwahati, India, KVNM Ramesh, PES University EC Campus, Bangalore, India"
         },
         {
             "Title": "Advances in Fluid Flow and Heat Transfer Studies",
-            "SubmissionDeadLine":"30 April 2024",
+            "SubmissionDeadLine":"31 December 2024",
             "File":"https://res.cloudinary.com/jisst/image/upload/v1704705591/Special_Issue-Fluid_Flows_Dead_Line_30_April_2024_icadvd.pdf",
             "Editors":"T.S.L. Radhika, BITS Pilani, Hyderabad campus, India, Jyotirmoy Rana, IIT Hyderabad, India, S. Srinivas, VIT-AP University, Amaravati, India"
         },
