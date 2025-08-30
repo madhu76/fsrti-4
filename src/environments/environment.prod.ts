f02875bbea233903fa82779530486a3bffafe79b
Sankar Mantripragada <mavsankar@gmail.com>
2025-08-30 20:04:16 +0530

    feat: update Supabase configuration in production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   supabase: {
-    url: 'YOUR_SUPABASE_URL', // Replace with your Supabase URL
-    anonKey: 'YOUR_SUPABASE_ANON_KEY' // Replace with your Supabase anon key
+    url: 'https://lgpljnwgpuojhixmmmwp.supabase.co', // Replace with your Supabase URL
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxncGxqbndncHVvamhpeG1tbXdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY1NTg2MTIsImV4cCI6MjA3MjEzNDYxMn0.OIuSKFet2BE8VwsTO5Eb4MwSVuXqE99UV4I2_UDxA-c' // Replace with your Supabase anon key
   }
 };
