e6f416c5f06416a4a4917a552e52521941cab50c
Sankar Mantripragada <mavsankar@gmail.com>
2025-09-05 19:32:15 +0530

    fix: add announcements data with themes, editors, and deadlines

--- src/app/journal/announcement_data.ts
+++ src/app/journal/announcement_data.ts
@@ -1,2 +1,23 @@
-export const announcements =[]
-  
+export const announcements = [
+    {
+        "theme": "Edge Intelligence for Internet of Things – Algorithms, Architectures and Applications",
+        "editors": [
+            "Shaik Qadeer, Muffakham Jah College of Engineering, Hyderabad, India",
+            "Layak Ali, Central University of Karnataka, Kalaburgi, India",
+            "Mohammed Yousuf Khan, Maulana Azad National Urdu University, Hyderabad, India",
+            "Jameel Ahamed, University of Bisha, Kingdom of Saudi Arabia"
+        ],
+        "link": "https://www.jisst.com/specialissues?vol_issue=Open",
+        "lastDate": "15 November 2025"
+    },
+    {
+        "theme": "Real World Dynamical Systems and Numerics",
+        "editors": [
+            "Ranjit Kumar Upadhyay, Indian Institute of Technology, Dhanbad, India",
+            "Dongpo Hu, Qufu Normal University, Shandong, China",
+            "Arni S.R. Srinivasa Rao, Division of Infectious Diseases, Medical College of Georgia, USA"
+        ],
+        "link": "https://www.jisst.com/specialissues?vol_issue=Open",
+        "lastDate": "15 November 2025"
+    }
+]
