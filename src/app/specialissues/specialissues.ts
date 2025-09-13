c3f69b63b741ff27a64914a35913964ce586a7f0
Sankar Mantripragada <mavsankar@gmail.com>
2025-09-13 19:34:22 +0530

    fix: update editor field to plural and remove outdated special issues

--- src/app/specialissues/specialissues.ts
+++ src/app/specialissues/specialissues.ts
@@ -36,45 +36,14 @@ const specialissues = {
             "Title":"Edge Intelligence for Internet of Things - Algorithms, Architectures and Applications",
             "SubmissionDeadLine":"15 November 2025",
             "File":"https://res.cloudinary.com/jisst/image/upload/v1757080238/Special%20Issues/SI-IOT_womtiy.pdf",
-            "Editor":"Shaik Qadeer, Muffakham Jah College of Engineering, Hyderabad, India, Layak Ali, Central University of Karnataka, Kalaburgi, India, Mohammed Yousuf Khan, Maulana Azad National Urdu University, Hyderabad, India, Jameel Ahamed, University of Bisha, Kingdom of Saudi Arabia"
+            "Editors":"Shaik Qadeer, Muffakham Jah College of Engineering, Hyderabad, India, Layak Ali, Central University of Karnataka, Kalaburgi, India, Mohammed Yousuf Khan, Maulana Azad National Urdu University, Hyderabad, India, Jameel Ahamed, University of Bisha, Kingdom of Saudi Arabia"
         },
         {
             "Title": "Real World Dynamical Systems and Numerics",
             "SubmissionDeadLine":"15 November 2025",
             "File":"https://res.cloudinary.com/jisst/image/upload/v1757080244/Special%20Issues/SI-Dynamical_Systems_wrr2bf.pdf",
             "Editors":"Ranjit Kumar Upadhyay, Indian Institute of Technology, Dhanbad, India, Dongpo Hu, Qufu Normal University, Shandong,  China, Arni S.R. Srinivasa Rao, Division of Infectious Diseases, Medical College of Georgia, USA"
-        },
-        {
-            "Title": "Advances in Partial Differential Equations:Theory, Applications and Numerics",
-            "SubmissionDeadLine":"31 December 2024",
-            "File":"https://res.cloudinary.com/jisst/image/upload/v1704815686/S_ecial_Issue_on_PDE_s_jjjsec.pdf",
-            "Editors":"Neelesh Kumar, Dyal Singh College, University of Delhi, India, Pankaj Mishra, Deshbandhu College, University of Delhi, India"
-        },
-        {
-            "Title": "Financial Engineering - Theory, Models and Software Implementations",
-            "SubmissionDeadLine":"31 December 2024",
-            "File":"https://res.cloudinary.com/jisst/image/upload/v1704705354/SI-Fiancial_Engg._-_Dead_line_30_April_2024_nt5ngd.pdf",
-            "Editors":"Siddhartha Pratim Chakrabarty, Indian Institute of Technology Guwahati, India, KVNM Ramesh, PES University EC Campus, Bangalore, India"
-        },
-        {
-            "Title": "Advances in Fluid Flow and Heat Transfer Studies",
-            "SubmissionDeadLine":"31 December 2024",
-            "File":"https://res.cloudinary.com/jisst/image/upload/v1704705591/Special_Issue-Fluid_Flows_Dead_Line_30_April_2024_icadvd.pdf",
-            "Editors":"T.S.L. Radhika, BITS Pilani, Hyderabad campus, India, Jyotirmoy Rana, IIT Hyderabad, India, S. Srinivas, VIT-AP University, Amaravati, India"
-        },
-        {
-            "Title": "Advances in Fractional Calculus: Theory, Models and Applications",
-            "SubmissionDeadLine":"31 December 2024",
-            "File":"https://res.cloudinary.com/jisst/image/upload/v1721872290/Advances_in_Fractional_Calculus_Theory_Models_and_Applications_ekc1lx.pdf",
-            "Editors":"Rajesh K. Pandey, Indian Institute of Technology (BHU) Varanasi, India, Yufeng Xu, Central South University, Changsha, Hunan, P.R. China, Arvind K Misra, Banaras Hindu University, Varanasi, Uttar Pradesh, India"
-        },
-        {
-            "Title": "Synergizing Intelligence: Harnessing Advanced Data Technologies through Innovative Machine Learning Applications",
-            "SubmissionDeadLine":"31 December 2024",
-            "File":"https://res.cloudinary.com/jisst/image/upload/v1722137861/Synergizing_Intelligence_kjllma.pdf",
-            "Editors":"Manoj Sahni, Pandit Deendayal Energy University, Gandhinagar, Gujarat, India, G. Madhu, VNR Vignana Jyothi Institute of Engineering & Technology, Hyderabad, India, Ernesto Leon Castro, Universidad Católica de la Santísima Concepcion, Chile, Ritu Sahni, Foundation for Scientific Research and Technological Innovation, Hyderabad, India"
         }
-      
         
     ]
 }
